863fc267fde701cca50746aee1b0d99b5ae30068
mmarcog1 <mmarcog1@gmx.com>
2019-03-29 00:56:25 +0100

    Added heroku deploy configs

--- config/prod.exs
+++ config/prod.exs
@@ -21,6 +21,7 @@ config :logger, level: :info
 
 # Configure your database
 config :prototype, Prototype.Repo,
+  adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true

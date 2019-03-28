504b137eebef682f77cd9f8cb412806420b04670
mmarcog1 <mmarcog1@gmx.com>
2019-03-29 00:17:35 +0100

    Use production config from Heroku ENV variables and decrease socket timeout

--- lib/prototype_web/endpoint.ex
+++ lib/prototype_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule PrototypeWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :prototype
 
   socket "/socket", PrototypeWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.

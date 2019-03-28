6deeb9417bbaf31fb553b8ebceac2b6108de2406
mmarcog1 <mmarcog1@gmx.com>
2019-03-29 00:48:43 +0100

    Added migration

--- config/prod.exs
+++ config/prod.exs
@@ -20,7 +20,7 @@ config :hello, HelloWeb.Endpoint,
 config :logger, level: :info
 
 # Configure your database
-config :hello, Hello.Repo,
+config :prototype, Prototype.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
